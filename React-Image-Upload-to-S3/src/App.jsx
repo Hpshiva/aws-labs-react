b0afabb0b544d21f8c0168ba731a8435b7c55521
Hpshiva <shivakumar.bfgi@gmail.com>
2026-05-24 22:40:06 +0530

    with new feature

--- React-Image-Upload-to-S3/src/App.jsx
+++ React-Image-Upload-to-S3/src/App.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import "./index.css";
 import Card from "./component/Card";
+import axios from "axios";
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState([]);
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [imgSuccess, SetImgsuccess] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [preview, setPreview] = useState("");
 
   //useEffect here to load images on every render/reload of the page
   // as before it's not getting load the images after upload only load the images
@@ -16,8 +19,9 @@ function App() {
     );
     const imagesData = await imagesRes.json();
     setImages(imagesData);
-    console.log(imagesData);
+    // console.log(imagesData);
   };
+
   const getUploadUrl = async () => {
     if (!file) return;
     setLoading(true);
@@ -28,17 +32,30 @@ function App() {
     const data = await res.json();
     console.log(data);
 
-    // Step 2: Upload image to S3
-    await fetch(data.uploadUrl, {
-      method: "PUT",
+    await axios.put(data.uploadUrl, file, {
       headers: {
         "Content-Type": file.type,
       },
-      body: file,
+      onUploadProgress: (event) => {
+        const percentage = Math.round((event.loaded / event.total) * 100);
+        setProgress(percentage);
+        // console.log(percentage);
+      },
     });
+
+    // Step 2: Upload image to S3
+    // await fetch(data.uploadUrl, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": file.type,
+    //   },
+    //   body: file,
+    // });
 
     SetImgsuccess(true);
     setLoading(false);
+
+    await fetchImages();
   };
 
   useEffect(() => {
@@ -50,39 +67,83 @@ function App() {
       <div className="h-full w-full bg-gray-600 text-white flex flex-col gap-5  py-[5rem] items-center">
         <h1 className="text-4xl">S3 Upload App</h1>
 
-        <div className="card rounded-md p-[3rem] border shadow-dm bg-gray-300 h-[5rem] w-fit flex justify-center items-center ">
-          <input
-            type="file"
-            onChange={(event) => {
-              console.log(event);
-              setFile(event.target.files[0]);
-            }}
-            placeholder="Upload You File Here"
-            className="text-black"
-          />
-        </div>
+        <div className="card rounded-md p-[2rem] border shadow-dm bg-gray-300 h-auto md:h-[20rem] w-[60%] md:flex gap-5 justify-between items-center ">
+          <div className="card rounded-md p-[1rem] shadow-dm bg-gray-500 h-[14rem] md:h-full w-[100%] ">
+            <img
+              src={preview}
+              alt=""
+              className="h-full rounded-md object-cover w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <input
+              multiple
+              type="file"
+              onChange={(event) => {
+                console.log(event);
+                console.log(event.target.files);
 
-        <button
-          onClick={getUploadUrl}
-          disabled={loading}
-          className="rounded-md px-4 py-2 border shadow-dm bg-green-300 text-black"
-        >
-          {loading ? "Uploading..." : "Upload Image.."}
-        </button>
-        {imgSuccess && (
-          <p
-            id="text-status"
-            className=" text-green-900 font-bold bg-green-300 px-4 py-2 rounded-md"
-          >
-            Image Uploaded Successfully
-          </p>
-        )}
+                setFile(event.target.files);
+                console.log("file",file);
+                const selectedFile = event.target.files;
+                // console.log(URL.createObjectURL(selectedFile));
+                const previewUrl = [];
+                for (let i = 0; i <= selectedFile.length; i++) {
+                  previewUrl[i] = URL.createObjectURL(selectedFile[i]);
+                }
+
+                setPreview(previewUrl);
+              }}
+              placeholder="Upload You File Here"
+              className="text-black"
+            />
+
+            <button
+              onClick={getUploadUrl}
+              disabled={loading}
+              className="rounded-md px-4 py-2 border shadow-dm bg-green-300 text-black"
+            >
+              {loading ? "Uploading..." : "Choose Image..."}
+            </button>
+
+            {loading && (
+              <p className=" text-green-900 font-bold bg-yellow-300 px-4 py-2 text-center rounded-md">
+                {progress}%
+              </p>
+            )}
+            {loading && (
+              <div className="outter h-[1rem] bg-gray-500 w-full md:w-[26rem] rounded-md">
+                <div
+                  className="inner h-[100%] rounded-md bg-green-500"
+                  style={{ width: `${progress}%` }}
+                ></div>
+              </div>
+            )}
+
+            {imgSuccess && (
+              <p
+                id="text-status"
+                className=" text-green-900 font-bold bg-green-300 px-4 py-2 rounded-md"
+              >
+                Image Uploaded Successfully
+              </p>
+            )}
+          </div>
+        </div>
 
         <p>{file?.name}</p>
 
         <div className="w-[90%] bg-gray-400 rounded-md p-4 gap-5 grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
           {images.map((items) => {
-            return <Card name={items.key} url={items.url} />;
+            return (
+              <Card
+                key={items.key}
+                name={items.key}
+                url={items.url}
+                imageKey={items.key}
+                fetchImages={fetchImages}
+              />
+            );
           })}
         </div>
       </div>
--- React-Image-Upload-to-S3/src/component/Card.jsx
+++ React-Image-Upload-to-S3/src/component/Card.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-function Card({ name, url }) {
+function Card({ name, url, imageKey, fetchImages }) {
+  const deleteImage = async () => {
+    console.log(url);
+    await fetch(
+      `https://jfcw2te5tzny2c63y46wvo36m40iqpws.lambda-url.us-east-1.on.aws/?type=delete&key=${imageKey}`,
+    );
+    await fetchImages();
+  };
   return (
     <div className="bg-gray-500 w-full h-fit  border border-default rounded-md shadow-xs">
       <img
@@ -8,10 +15,17 @@ function Card({ name, url }) {
         alt=""
         className="w-full rounded-md object-cover h-[10rem] sm:h-[14rem] lg:h-[18rem]"
       />
-      <div className="p-3 text-center">
-        <h5 className="my-4 lg:text-2xl font-semibold tracking-tight text-heading">
+      <div className="p-4 text-center flex  flex-col gap-5">
+        <h5 className="lg:text-2xl font-semibold tracking-tight text-heading">
           {name}
         </h5>
+        <button
+          onClick={deleteImage}
+          className="bg-red-500 rounded-full px-6 py-2"
+        >
+          {" "}
+          Delete{" "}
+        </button>
       </div>
     </div>
   );
